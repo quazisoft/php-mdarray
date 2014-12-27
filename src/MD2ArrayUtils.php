906689060fd1a8ffd1cbdd2cdbdef831574079f8
Victor V. Borisov <quazer.ru@gmail.com>
2014-12-27 15:01:05 -0300

    + MD2ArrayUtils is row or column empty

---
+++ src/MD2ArrayUtils.php
@@ -0,0 +1,52 @@
+<?php
+namespace Quazisoft\MDArray;
+
+/**
+ * array[][] utils
+ *
+ * @author Victor Borisov (quazer.ru@gmail.com)
+ *
+ */
+class MD2ArrayUtils
+{
+
+    /**
+     * Is column has only null values
+     *
+     * @param array $array
+     * @param int $row
+     * @return boolean
+     */
+    public static function isNullRow($array, $row)
+    {
+        $isNull = true;
+        for ($column = 0; $column < count($array[$row]); $column ++) {
+            if (null !== $array[$row][$column]) {
+                $isNull = false;
+                break;
+            }
+        }
+        return $isNull;
+    }
+
+    /**
+     * Is column has only null values
+     *
+     * @param array $array
+     * @param int $column
+     * @return boolean
+     */
+    public static function isNullColumn($array, $column)
+    {
+        $isNull = true;
+        for ($row = 0; $row < count($array); $row ++) {
+            if (null !== $array[$r][$column]) {
+                $isNull = false;
+                break;
+            }
+        }
+        return $isNull;
+    }
+}
+
+?>
